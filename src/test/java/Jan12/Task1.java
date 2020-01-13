4993b9022c5e08548c4f317643c7e63634abdccf
naksbro <m.admana89@gmail.com>
2020-01-13 14:26:35 -0500

    More testNG

---
+++ src/test/java/Jan12/Task1.java
@@ -0,0 +1,65 @@
+package Jan12;
+
+import com.github.javafaker.Faker;
+import io.techleadacademy.ReusableMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Task1 {
+
+    ReusableMethods methods = new ReusableMethods();
+    WebDriver driver = methods.webDriverSetUp("https://www.phptravels.net/index.php");
+    Faker faker = new Faker();
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String phnNum = faker.phoneNumber().cellPhone();
+    String email = firstName+lastName+"@gmail.com";
+    String password = "drowssap";
+    LocalDate date = LocalDate.now();
+    DateTimeFormatter df = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+    String today = df.format(date);
+
+    @Test
+    public void _001verifyTitle () {
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "PHPTRAVELS | Travel Technology Partner";
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void _002verifySignUp () {
+        String signUpPage = driver.getTitle();
+
+        driver.findElement(By.xpath("(//a[@id='dropdownCurrency'])[2]")).click();
+        driver.findElement(By.xpath("//a[@class='dropdown-item tr']")).click();
+        driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys(firstName);
+        driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys(lastName);
+        driver.findElement(By.xpath("//input[@name='phone']")).sendKeys(phnNum);
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@name='confirmpassword']")).sendKeys(password);
+        driver.findElement(By.xpath("(//button[@type='submit'])[1]")).click();
+
+        String profilePage = driver.getTitle();
+        Assert.assertNotEquals(signUpPage, profilePage);
+    }
+
+    @Test
+    public void _003verifyProfilePage () {
+        methods.sleep(5000);
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "My Account";
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        String actualHi = driver.findElement(By.xpath("//h3[@class='text-align-left']")).getText();
+        String expectedHi = "Hi, "+firstName+" "+lastName;
+        Assert.assertTrue(actualHi.equals(expectedHi));
+
+        String actualDate = driver.findElement(By.xpath("//span[@class='h4']")).getText();
+        Assert.assertEquals(actualDate, today);
+    }
+}
--- src/test/java/io/techleadacademy/ReusableMethods.java
+++ src/test/java/io/techleadacademy/ReusableMethods.java
@@ -1,11 +1,14 @@
 package io.techleadacademy;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class ReusableMethods {
     public void sleep(int milliSeconds){
@@ -22,5 +25,14 @@ public class ReusableMethods {
     public void waitForVisibilityOfAll(List<WebElement> list, WebDriver driver){
         WebDriverWait explicitWait = new WebDriverWait(driver, 10);
         explicitWait.until(ExpectedConditions.visibilityOfAllElements(list));
+    }
+
+    public WebDriver webDriverSetUp (String url) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
     }
 }
